a9c668ab4a473f73389c7ad4399a42c8804360f6
ksm0520 <kimseungmin@gmail.com>
2025-06-06 15:16:57 +0900

    chore:railway 서버 기초 세팅

---
+++ Server/src/index.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import routinesRouter from './routes/routines';
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use('/routines', routinesRouter);
+
+app.listen(port, () => {
+  console.log(`🚀 Server is running at http://localhost:${port}`);
+});
